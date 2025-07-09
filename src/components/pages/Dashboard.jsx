5a151de97f8655e20b0cdd4e74e091aa6ce441d4
Yashmevada2807 <yashmevada07@gmail.com>
2025-07-09 10:32:03 +0530

    added Dashboard page

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
-      
+    <div className='w-full min-h-screen bg-gray-950  '>
+        <div className="header flex justify-between items-center border-b border-gray-600 px-6 w-full bg-[#180323]  py-6">
+            <h1 className='text-gray-300 text-3xl'>Hello <span className='uppercase'>User</span></h1>
+            <button className='bg-orange-500 px-5 py-2 rounded-md text-gray-100'>LogOut</button>
+        </div>
+        <div className="content flex justify-center items-center p-4">
+            <p className='text-5xl flex justify-center py-10 items-center text-white'>Products Will Be Coming Soon...</p>
+        </div>
     </div>
   )
 }
