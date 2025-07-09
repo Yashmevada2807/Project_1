6646da31bbd57267f7fa0e54d8f40a6245be7545
Yashmevada2807 <yashmevada07@gmail.com>
2025-07-09 15:19:43 +0530

    added provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import { useState } from 'react'
 import SignInPage from './components/pages/SignInPage'
 import LoginPage from './components/pages/LoginPage'
 import Dashboard from './components/pages/Dashboard'
-
-
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function App() {
+  
+  const { isAuthenticated } = useSelector(state => state.users)
 
   return (
-    <>
-     <div className='w-full min-h-screen flex justify-center items-center bg-gray-950'>
-      {/* <SignInPage/> */}
-      {/* <LoginPage/> */}
-      <Dashboard/>
-     </div>
-    </>
+
+    <div className='w-full min-h-screen flex justify-center items-center bg-gray-950'>
+      <Routes>
+        <Route path='/' element={isAuthenticated ? <Navigate to='/dashboard'/> : <LoginPage />} />
+        <Route path='/signin' element={isAuthenticated ? <Dashboard/> : <SignInPage />} />
+        <Route path='/dashboard' element={isAuthenticated ? <Dashboard /> : <Navigate to='/'/>} />
+      </Routes>
+    </div>
+
   )
 }
 
