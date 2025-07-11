bbb9dd11e75c385e928701f36cd135af473f4dc0
Yashmevada2807 <yashmevada07@gmail.com>
2025-07-11 10:34:07 +0530

    added productDetail Page

---
+++ src/components/pages/ProductDetailPage.jsx
@@ -0,0 +1,110 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+
+const ProductDetailPage = () => {
+
+    const { id } = useParams()
+    const [product, setProduct] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchProductDetailById = async () => {
+            const res = await axios.get(`https://dummyjson.com/products/${id}`)
+            setProduct(res.data)
+            setIsLoading(false)
+        }
+        fetchProductDetailById()
+    }, [id])
+
+
+
+    if (isLoading) return <p className='text-6xl text-white flex justify-center items-center'>Loading product...</p>;
+
+    if (!product) return <p className='text-6xl text-white flex justify-center items-center'>No product found.</p>;
+
+
+    const discountedPrice = product.price * [product.discountPercentage / 100]
+    const finalPrice = product.price - discountedPrice
+    return (
+        <div className='w-full  p-2 '>
+            <div className=" w-full p-8 grid grid-cols-1 md:grid-cols-2  gap-8 bg-[#250e32]">
+
+                <div className=''>
+                    <img
+                        src={product.images[0]}
+                        alt={product.title}
+                        className="w-full h-full  p-10 rounded-lg object-cover bg-[#060616]"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <h2 className="text-2xl text-gray-300 font-semibold">{product.title}</h2>
+
+                    <div className="flex items-center mt-2">
+                        {[1, 2, 3, 4, 5].map((star) => (
+                            <svg
+                                key={star}
+                                className={`w-5 h-5 ${product.rating >= star ? "text-yellow-400" : "text-gray-500"
+                                    }`}
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.955a1 1 0 00.95.69h4.162c.969 0 1.371 1.24.588 1.81l-3.37 2.448a1 1 0 00-.364 1.118l1.287 3.955c.3.921-.755 1.688-1.538 1.118l-3.37-2.448a1 1 0 00-1.175 0l-3.37 2.448c-.783.57-1.838-.197-1.539-1.118l1.287-3.955a1 1 0 00-.364-1.118L2.074 9.382c-.783-.57-.38-1.81.588-1.81h4.162a1 1 0 00.95-.69l1.286-3.955z" />
+                            </svg>
+                        ))}
+                    </div>
+                    <p className="mt-4 text-gray-500">{product.description}</p>
+
+                    {/* <div className="mt-6">
+                        <h4 className="text-sm font-medium text-gray-900">Color</h4>
+                        <div className="flex items-center mt-2 space-x-2">
+                            {colors.map((c, i) => (
+                                <button
+                                    key={i}
+                                    className="w-6 h-6 rounded-full border border-gray-300"
+                                    style={{ backgroundColor: c }}
+                                />
+                            ))}
+                        </div>
+                    </div> */}
+
+
+
+                    <div className="mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2">
+                        <div className="border-[1px] border-[#842093] p-4 rounded-md">
+                            <h4 className="text-sm font-semibold text-gray-300">Brand</h4>
+                            <p className="mt-1 text-sm text-gray-400">{product.brand}</p>
+                        </div>
+                        <div className="border-[1px] border-[#842093] p-4 rounded-md">
+                            <h4 className="text-sm font-semibold text-gray-300">Category</h4>
+                            <p className="mt-1 text-sm text-gray-400 capitalize">{product.category}</p>
+                        </div>
+                        <div className="border-[1px] border-[#842093] p-4 rounded-md">
+                            <h4 className="text-sm font-semibold text-gray-300">In Stock</h4>
+                            <p className="mt-1 text-sm text-gray-400">{product.stock} units</p>
+                        </div>
+                        <div className="border-[1px] border-[#842093] p-4 rounded-md">
+                            <h4 className="text-sm font-semibold text-gray-300">Discount</h4>
+                            <p className="mt-1 text-sm text-green-500">{product.discountPercentage}% Off</p>
+                        </div>
+                    </div>
+                    <p className="text-3xl text-gray-400 font-semibold mt-4 ">$<span className='line-through'>{product.price}</span> <span className='text-[15px]  text-green-400 font-light'>- {product.discountPercentage}% Off</span></p>
+                    <p className="text-2xl px-1 font-bold pt-2  text-blue-500 "> ${finalPrice.toFixed(2)}<span className='text-sm  text-gray-400 font-light'> Including tax</span></p>
+                    <div className='w-full pt-12 flex'>
+                        <button className="mt-6 w-1/2 bg-[#842093] hover:bg-[#6e3f75] hover:text-gray-100 text-white px-6 py-3">
+                            Buy Now
+                        </button>
+                        <button className="mt-6 w-1/2 bg-gray-700 text-white px-6 py-3  hover:bg-[#383c56]">
+                            Add to bag
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductDetailPage
