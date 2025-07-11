81071794bce90fb37408ab1eb4493dc62a0f7ffc
Yashmevada2807 <yashmevada07@gmail.com>
2025-07-11 10:34:46 +0530

    added product Page

---
+++ src/components/pages/ProductPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductList from '../ProductList'
+
+const ProductPage = () => {
+  return (
+    <>
+      <div className='w-full min-h-screen py-6 flex justify-center '>
+        <ProductList/>
+      </div>
+    </>
+  )
+}
+
+export default ProductPage
