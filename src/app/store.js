97b8c2a3d1c9947fcd1218c264326ded143f0084
Yashmevada2807 <yashmevada07@gmail.com>
2025-07-11 10:31:08 +0530

    added persist logic in store to persist user data

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { userReducer } from "../features/product/productSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { productReducer, userReducer } from "../features/product/productSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+
+const usersPersistConfig = {
+    key: 'users',
+    storage,
+}
+
+const rootReducer = combineReducers({
+    users: persistReducer(usersPersistConfig, userReducer),
+    products: productReducer,
+})
+
+
 
 
 export const store = configureStore({
-    reducer: {
-        users: userReducer
-    }
+    reducer: rootReducer,
 })
+export const persistor = persistStore(store)
