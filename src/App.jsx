519156207e107bcaf31f88f7b6e8e02d550e6a94
Yashmevada2807 <yashmevada07@gmail.com>
2025-07-14 10:24:45 +0530

    sm changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,28 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import ProductPage from './components/pages/ProductPage'
 import ProductDetailPage from './components/pages/ProductDetailPage'
+import HeaderSection from './components/HeaderSection'
+import LogOutModal from './components/modals/LogOutModal'
+import ProductCartPage from './components/pages/ProductCartPage'
 
 function App() {
-  
+
   const { isAuthenticated } = useSelector(state => state.users)
 
   return (
 
-    <div className='w-full min-h-screen flex justify-center items-center bg-gray-950'>
+    <div className='w-full min-h-screen flex flex-col bg-gray-950'>
+      <div className="header w-full fixed z-12  top-0 ">
+        {isAuthenticated ? <HeaderSection/> : null}
+      </div>
       <Routes>
-        <Route path='/' element={isAuthenticated ? <Navigate to='/dashboard'/> : <LoginPage />} />
-        <Route path='/signin' element={isAuthenticated ? <Dashboard/> : <SignInPage />} />
-        <Route path='/dashboard' element={isAuthenticated ? <Dashboard /> : <Navigate to='/'/>} />
-        <Route path={`/products/:id`}id element={<ProductDetailPage/>}/>
+        <Route path='/' element={isAuthenticated ? <Navigate to='/dashboard' /> : <LoginPage />} />
+        <Route path='/signin' element={isAuthenticated ? <Dashboard /> : <SignInPage />} />
+        <Route path='/dashboard' element={isAuthenticated ? <Dashboard /> : <Navigate to='/' />} />
+        <Route path={`/products/:id`} element={isAuthenticated ? <ProductDetailPage /> : <Navigate to='/' />} />
+        <Route path={`/cart`} element={isAuthenticated ? <ProductCartPage/> : null} />
       </Routes>
+      
     </div>
 
   )
