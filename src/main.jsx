7cbf851eabbec28be5046f835e763902dc8231ba
Yashmevada2807 <yashmevada07@gmail.com>
2025-07-15 10:22:43 +0530

    sm changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,12 @@ import { store } from './app/store.js'
 import { BrowserRouter } from 'react-router-dom'
 import { persistor } from './app/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
+import { Bounce, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './style.css'
+
+
+
 
 createRoot(document.getElementById('root')).render(
 
@@ -14,6 +20,18 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={true}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          theme='colored'
+          transition={Bounce}
+        />
       </PersistGate>
     </Provider>
   </BrowserRouter>
