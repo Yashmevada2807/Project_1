7c57deb0748217348d3691c4ab2a2748fb20eb96
Yashmevada2807 <yashmevada07@gmail.com>
2025-07-16 11:43:43 +0530

    sm changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import LoginPage from './components/pages/LoginPage'
 import Dashboard from './components/pages/Dashboard'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import ProductPage from './components/pages/ProductPage'
 import ProductDetailPage from './components/pages/ProductDetailPage'
 import HeaderSection from './components/HeaderSection'
-import LogOutModal from './components/modals/LogOutModal'
 import ProductCartPage from './components/pages/ProductCartPage'
+
 
 function App() {
 
@@ -16,7 +15,7 @@ function App() {
 
   return (
 
-    <div className='w-full min-h-screen flex flex-col bg-gray-950'>
+    <div className='w-full min-h-screen flex flex-col justify-center items-center  bg-gray-950'>
       <div className="header w-full fixed z-12  top-0 ">
         {isAuthenticated ? <HeaderSection/> : null}
       </div>
@@ -27,7 +26,6 @@ function App() {
         <Route path={`/products/:id`} element={isAuthenticated ? <ProductDetailPage /> : <Navigate to='/' />} />
         <Route path={`/cart`} element={isAuthenticated ? <ProductCartPage/> : <Navigate to='/'/>} />
       </Routes>
-      
     </div>
 
   )
